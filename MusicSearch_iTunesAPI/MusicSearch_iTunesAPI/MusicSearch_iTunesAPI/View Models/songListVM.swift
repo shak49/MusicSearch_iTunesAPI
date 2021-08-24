c3e96341f4c70a5d3bf5465414e561ae93912d3a
shak49 <74379616+shak49@users.noreply.github.com>
2021-08-24 14:29:28 -0600

    Add files via upload

---
+++ MusicSearch_iTunesAPI/MusicSearch_iTunesAPI/MusicSearch_iTunesAPI/View Models/songListVM.swift
@@ -0,0 +1,81 @@
+//
+//  ItemListVM.swift
+//  MusicSearch_iTunesAPI
+//
+//  Created by Shak Feizi on 8/21/21.
+//
+
+import SwiftUI
+import Combine
+
+
+class SongVM: Identifiable, ObservableObject {
+    let id: Int
+    let artist: String
+    let track: String
+    let price: Double
+    let releaseDate: String
+    let genre: String
+    @Published var artistImage: Image?
+
+    init(song: Song) {
+        self.id = song.artistId
+        self.artist = song.artistName
+        self.track = song.trackName
+        self.price = song.trackPrice
+        self.releaseDate = song.releaseDate
+        self.genre = song.primaryGenreName
+    }
+}
+
+
+class SongListVM: ObservableObject {
+    // Shak notes: Properties
+    static let sharedSongListVM: SongListVM = SongListVM()
+    @Published var searchTerm: String = ""
+    @Published var songsArray: [SongVM] = []
+    @Published var isLoading = false
+    private var resultController: ResultsController = ResultsController()
+    private var song: Song?
+    private var desposable = Set<AnyCancellable>()
+    
+    init() {
+        $searchTerm
+            .sink(receiveValue: fetchDataForView(searchTerms:))
+            .store(in: &desposable)
+    }
+    
+    // Shak notes: Funtions
+    func fetchDataForView(searchTerms: String?) {
+        songsArray.removeAll()
+        isLoading = true
+        //self.resultController.reset()
+        guard let searchTerm = searchTerms else { return }
+        iTunesNetworkCall.sharedInstance.fetchSongs(searchTerm: searchTerm) { results in
+            switch results {
+            case .success(let songs):
+                songs.forEach { self.appendSong(song: $0) }
+                print(self.songsArray)
+            case .failure(let error):
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+            self.isLoading = false
+        }
+    }
+    
+    func appendSong(song: Song?) {
+        //isLoading = true
+        guard let song = song else { return }
+        let songVM = SongVM(song: song)
+        DispatchQueue.main.async {
+            self.songsArray.append(songVM)
+            //self.isLoading = false
+        }
+        resultController.fetchImage(song: song) { image in
+            DispatchQueue.main.async {
+                songVM.artistImage = image
+            }
+        }
+    }
+    
+}
